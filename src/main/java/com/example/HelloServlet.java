a89493c55d319dfbd2d021dd5aa638ff8b84f913
imran <imranshaik99660@gmail.com>
2025-08-06 12:34:58 +0530

    Initial commit

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body><h1>Hello from Dockerized Java App!</h1></body></html>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+  <body>
+    <h2>Hello from JSP!</h2>
+    <p>Welcome to your Dockerized Java Web App.</p>
+  </body>
+</html>
